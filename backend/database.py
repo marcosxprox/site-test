3e801dc63f9cc543bf72e2ac86f2c5974af20a8c
MarcosOli <marcos.xprox@gmail.com>
2025-08-17 09:54:20 -0300

    pymysql

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
 import os
+import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+pymysql.install_as_MySQLdb()
 
 # Monta a URL de conexão
 DATABASE_URL = os.environ.get("DATABASE_URL")
