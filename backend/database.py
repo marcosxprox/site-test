2f1e18846cc8f13f6038b19a901c344699135f10
MarcosOli <marcos.xprox@gmail.com>
2025-08-17 09:47:12 -0300

    update database

--- backend/database.py
+++ backend/database.py
@@ -2,20 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-
-# Carrega as variáveis do .env
-load_dotenv()
-
-# Pega cada variável separadamente
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
 
 # Monta a URL de conexão
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Cria o engine do SQLAlchemy
 engine = create_engine(DATABASE_URL, echo=True)
